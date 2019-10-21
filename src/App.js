dad986fe099fd3646e5ed16be806dc1aac85bf7a
pyankovit <pyankov.it@gmail.com>
2019-10-21 22:47:58 +0300

    added model filter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect }  from 'react';
 import './App.css';
 
+
 function App() {
+  const [term, setterm] = useState('');
+  const [cars, setcars] = useState([
+  {
+    id:1,
+    brand: "honda",
+    model: "civic",
+    year: "1996"
+  },
+  {
+    id:2,
+    brand: "dodge",
+    model: "charger",
+    year: "1968"
+  },
+  {
+    id:3,
+    brand: "toyota",
+    model: "supra",
+    year: "1995"
+  }
+  ]);
+
+  const handleSearchInputChanges = (e) => {
+    setterm(e.target.value);
+  }
+  const searchingFor=term=>x=>x.brand.includes(term)||x.model.includes(term)||!term;
+ 
+  const mappedCars=cars.filter(searchingFor(term)).map(car=>
+  <div className="car-card" key={car.id}>
+    <h1>{car.brand}</h1>
+    <h1>{car.model}</h1>
+    <h3>{car.year}</h3>
+  </div>);
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input 
+        type="text"
+        onChange={handleSearchInputChanges}
+        value={term}
+        placeholder="carname..."
+        />
+      </form>
+      {mappedCars}
     </div>
   );
 }
